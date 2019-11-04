e7f2977776a93da7f29330a72bd37317f39f4e7f
BrandonMGH <BrandonMGH@Yahoo.com>
2019-11-03 19:01:07 -0800

    added a new socket.io connection function

--- server/index.js
+++ server/index.js
@@ -2,13 +2,20 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const router = require("./router.js");
 
 const app = express(); 
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on("connection", (socket) =>{
+    console.log("We have a new connection!!!")
+    socket.on("disconnect", () => {
+        console.log("User has disconnected!!!")
+    })
+})
 
 app.use(router);
 

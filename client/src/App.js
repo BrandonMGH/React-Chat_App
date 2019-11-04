80e0f82c4580e921ebc077c1d9ae68284579cea0
BrandonMGH <BrandonMGH@Yahoo.com>
2019-11-03 20:21:17 -0800

    incorporated some basic styling to join and chat components

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 //** COMPONENTS  **//
-import Join from './components/Join.js';
-import Chat from './components/Chat.js';
+import Join from './components/Join/Join.js';
+import Chat from './components/Chat/Chat.js';
 
 export default function App() {
     return (

031c6ed1b186528e163e3532a3ad745379507e66
BrandonMGH <BrandonMGH@Yahoo.com>
2019-11-05 15:06:32 -0800

    was able to send a socket.io request from the index.js on the client side to the server.js on the server side

--- client/index.js
+++ client/index.js
@@ -1,13 +1,23 @@
-import React from "react"
-import ReactDom from "react-dom"
-import Button from '@material-ui/core/Button';
+import socketIOClient from "socket.io-client"
 
-function Index() {
-  return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-  );
-}
+const socket = socketIOClient ('http://localhost:3000/'); 
+socket.on('connect', function () {
+  socket.send("hi");
 
-ReactDom.render(<Index />, document.getElementById("root"))
+  socket.on("message", function (msg){
+
+  }); 
+});
+// import React from "react"
+// import ReactDom from "react-dom"
+// import Button from '@material-ui/core/Button';
+
+// function Index() {
+//   return (
+//     <Button variant="contained" color="primary">
+//       Hello World
+//     </Button>
+//   );
+// }
+
+// ReactDom.render(<Index />, document.getElementById("root"))
--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require("express")
 const app = express(); 
 const PORT = 3000
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.get('/', (req,res) =>{
-    res.send("Hello World")
-})
+io.on("connection", function (socket) {
+    console.log("Connected established!");
+    socket.on("message", function (data) {
+        console.log("Message received:", data)
+    });
+    socket.on("disconnect", function (){});
+});
 
-app.listen(PORT, () => {
-    console.log("Listening on PORT: " + PORT)
+http.listen(PORT, () => {
+    console.log(`Connected on Port: ${PORT}`)
 })

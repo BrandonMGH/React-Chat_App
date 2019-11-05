d86f4f27fd296583a6a28ad2f6ba7d658f7c1058
BrandonMGH <BrandonMGH@Yahoo.com>
2019-11-05 10:41:43 -0800

    starting project over from scratch

---
+++ client/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import ReactDom from "react-dom"
+
+
+
+
+
+
+export default function Index() {
+    return(
+       <div>
+        Test
+       </div>
+
+    )
+
+}
+
+ReactDom.render(<Index />, document.getElementById("root"))

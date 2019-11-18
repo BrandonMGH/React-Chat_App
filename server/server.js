2d11dd0da8918e8c1f4040be2340efb4900deb7b
BrandonMGH <BrandonMGH@Yahoo.com>
2019-11-18 09:47:10 -0800

    was having an issue where the socket.id I was accessing inside of the socket.on chat-message server event was different from the one I was receiving from the socket.on chat-name-server event.  Created a global id variable to handle this and pass this down to the getUser function

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const users = []
+let id = ""
 
 const getUser = (object, callback) => {
   callback(object)
@@ -14,19 +15,23 @@ io.on('connection', function(socket){
     socket.on('chat-name-server', name => {
       // users[socket.id] = name
       // console.log(name)   
-      let id = socket.id
+      id = socket.id
       let newUser = {id, name}
       users.push(newUser)
       // console.log(users)
       io.emit('chat-name-client', name)
     });
     socket.on('chat-message-server', function(chatText) {
-      let user = getUser(users, () =>{
-        return(users[0].id)
+      let user; 
+      console.log(id)
+      getUser(users, () =>{
+        for(let i=0; i < users.length; i++){
+          user = (users[i].id)
+        }
       })
       console.log(chatText, user)
-      // let textContent = (users[socket.id] + ":" + chatText)
-      io.emit('chat-message-client', chatText);
+      let textContent = (user + ":" + chatText)
+      io.emit('chat-message-client', textContent);
     });
     socket.on("disconnect", function (){
       console.log("Disconnected")

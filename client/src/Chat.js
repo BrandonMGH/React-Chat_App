08a12b5034eaaf065739d4d867f23ded2fc61319
BrandonMGH <BrandonMGH@Yahoo.com>
2019-11-21 11:35:20 -0800

    updated the styling on the Chat.js using material.ui

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,24 +1,42 @@
 import React, { useState, useEffect } from "react"
 import queryString from 'query-string';
 import socketIOClient from "socket.io-client";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+// ** CSS STYLES ** // 
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
 
 export default function Chat() {
     const [chatName, setChatName] = useState("")
     const [chatText, setChatText] = useState("")
     const [chatTextContainer, setChatTextContainer] = useState([])
     const socket = socketIOClient('https://react-socket-chat-app.herokuapp.com/')
+    const classes = useStyles();
 
-    
-    const chatSend = (chatText) =>{
+
+
+    const chatSend = (chatText) => {
         socket.emit('chat-message-server', chatText)
-        setChatText("");  
+        setChatText("");
     }
     const sendChatInfo = (event, callback) => {
         event.preventDefault();
         if (chatText) {
-            const {name}  = queryString.parse(location.search)
+            const { name } = queryString.parse(location.search)
             socket.emit('chat-name-server', (name))
-            
+
             callback(chatText)
         }
 
@@ -27,7 +45,7 @@ export default function Chat() {
 
     useEffect(() => {
 
-        const {name}  = queryString.parse(location.search)
+        const { name } = queryString.parse(location.search)
         socket.emit('chat-name-server', (name))
         setChatName(name)
 
@@ -35,9 +53,9 @@ export default function Chat() {
 
 
     useEffect(() => {
-       
+
         socket.on('chat-message-client', (textContent) => {
-            console.log(textContent)  
+            console.log(textContent)
             setChatTextContainer([...chatTextContainer, textContent])
         }, [chatTextContainer]);
         return () => {
@@ -46,21 +64,26 @@ export default function Chat() {
 
     }, [chatTextContainer])
     return (
-
-        <div>
-            <h1>Welcome {chatName}!</h1>
-            <div>
-                <ul>
-                  {chatTextContainer.map((text, index) =>
-                        <li key={index}>{text}</li>
-                    )}
-                </ul>
-            </div>
-            <div>
-                <input placeholder="Press enter to send text" value={chatText} onChange={event => setChatText(event.target.value)}
-                    onKeyPress={event => event.key === "Enter" ? sendChatInfo(event, chatSend) : null}
-                />
-            </div>
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={3}> </Grid>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper}>
+                        {chatTextContainer.map((text, index) =>
+                            <p key={index}>{text}</p>
+                        )}
+                    </Paper>
+                </Grid>
+                <Grid item xs={3}> </Grid>
+            </Grid>
+            <Grid container spacing={3}>
+                <Grid item xs={3}></Grid>
+                <Grid item xs={6}>
+                    <input placeholder="Press enter to send text" value={chatText} onChange={event => setChatText(event.target.value)}
+                        onKeyPress={event => event.key === "Enter" ? sendChatInfo(event, chatSend) : null}/>
+                </Grid>
+                <Grid item xs={3}></Grid>
+            </Grid>
         </div>
     )
 }
